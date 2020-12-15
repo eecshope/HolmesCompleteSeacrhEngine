a15aefe53c7bfa3abc84c92990c46d7cdb39f524
eecshope <35558662+eecshope@users.noreply.github.com>
2020-12-15 11:28:17 +0800

    reformat the code and fix some typo bugs

--- main.py
+++ main.py
@@ -1,50 +1,54 @@
 import _pickle as pkl
+
 import numpy as np
+
 from model import query
 
-raw_file = open('./data/chapter_cache.pkl','rb')
+raw_file = open('./data/chapter_cache.pkl', 'rb')
 dictlist = pkl.load(raw_file)
 raw_file.close()
 dictnum = len(dictlist)
 
-raw_file = open('./data/wordbag_cache.pkl','rb')
+raw_file = open('./data/wordbag_cache.pkl', 'rb')
 wordbag = pkl.load(raw_file)
 raw_file.close()
 wordnum = len(wordbag)
 
-raw_file = open('./data/wordnumber_cache.pkl','rb')
+raw_file = open('./data/wordnumber_cache.pkl', 'rb')
 wordnumber = pkl.load(raw_file)
 raw_file.close()
 
-raw_file = open('./data/idf_cache.pkl','rb')
+raw_file = open('./data/idf_cache.pkl', 'rb')
 idf = pkl.load(raw_file)
 raw_file.close()
 
-raw_file = open('./data/tfidf_cache.pkl','rb')
+raw_file = open('./data/tfidf_cache.pkl', 'rb')
 tfidf = pkl.load(raw_file)
 raw_file.close()
 
-def cosine_similarity(x,y):
+
+def cosine_similarity(x, y):
     num = x.dot(y.T)
     denom = np.linalg.norm(x) * np.linalg.norm(y)
     return num / denom
 
-def handle_request(str_query,k):
-	q = query.Query(str_query)
-	q_tfidf = [0 for i in range(wordnum)]
-	for lemma in q.tf_count.keys():
-		if lemma in wordbag:
-			q_tfidf[wordnumber[lemma]] = q.tf_count[lemma]*idf[wordnumber[lemma]]
 
-	res = [[0,0] for _ in range(dictnum)]
-	for i in range(dictnum):
-		res[i][0] = cosine_similarity(np.array(q_tfidf),np.array(tfidf[i]))
-		res[i][1] = i
+def handle_request(local_str_query, k):
+    q = query.Query(local_str_query)
+    q_tfidf = [0] * wordnum
+    for lemma in q.tf_count.keys():
+        if lemma in wordbag:
+            q_tfidf[wordnumber[lemma]] = q.tf_count[lemma] * idf[wordnumber[lemma]]
 
-	res = sorted(res,key=(lambda x:x[0]),reverse=True)
-	for i in range(k):
-		print(dictlist[res[i][1]].story+" "+dictlist[res[i][1]].part+" "+dictlist[res[i][1]].chapter)
+    res = [[0, 0] for _ in range(dictnum)]
+    for i in range(dictnum):
+        res[i][0] = cosine_similarity(np.array(q_tfidf), np.array(tfidf[i]))
+        res[i][1] = i
+
+    res = sorted(res, key=(lambda x: x[0]), reverse=True)
+    for i in range(k):
+        print(dictlist[res[i][1]].story + " " + dictlist[res[i][1]].part + " " + dictlist[res[i][1]].chapter)
 
 
-str_query=input()
-handle_request(str_query,5)
+str_query = input()
+handle_request(str_query, 5)
--- vector_ir.py
+++ vector_ir.py
@@ -1,11 +1,9 @@
 import _pickle as pkl
-import numpy as np
-from model import chapter
-from model import query
-from model import inverted_unit
 import math
 
-raw_file = open('./data/chapter_cache.pkl','rb')
+from model import inverted_unit
+
+raw_file = open('./data/chapter_cache.pkl', 'rb')
 dictlist = pkl.load(raw_file)
 raw_file.close()
 
@@ -13,64 +11,50 @@ dictnum = len(dictlist)
 wordbag = set({})
 wordnumber = dict({})
 for i in range(dictnum):
-	for lemma in dictlist[i].tf_count.keys():
-		wordbag.add(lemma)
+    for lemma in dictlist[i].tf_count.keys():
+        wordbag.add(lemma)
 wordbag = list(wordbag)
 
-wordbag_file = open('./data/wordbag_cache.pkl','wb')
-pkl.dump(worbtag,worbtag_file)
+wordbag_file = open('./data/wordbag_cache.pkl', 'wb')
+pkl.dump(wordbag, wordbag_file)
 wordbag_file.close()
 
 wordnum = len(wordbag)
 inverted_index = list()
 
-
 for i in range(wordnum):
-	wordnumber[wordbag[i]] = i
+    wordnumber[wordbag[i]] = i
 
-wordnumber_file = open('./data/wordnumber_cache.pkl','wb')
-pkl.dump(wordnumber,wordnumber_file)
+wordnumber_file = open('./data/wordnumber_cache.pkl', 'wb')
+pkl.dump(wordnumber, wordnumber_file)
 wordnumber_file.close()
 
 idf = [0 for _ in range(wordnum)]
 tfidf = [[0 for _ in range(wordnum)] for __ in range(dictnum)]
 
 for lemma in wordbag:
-	doclist = list({})
-	for i in range(dictnum):
-		if lemma in dictlist[i].tf_count.keys():
-			idf[wordnumber[lemma]] += 1
-			doclist.append(i)
-	inverted_index.append(inverted_unit.Inverted_unit(doclist))
+    doclist = list({})
+    for i in range(dictnum):
+        if lemma in dictlist[i].tf_count.keys():
+            idf[wordnumber[lemma]] += 1
+            doclist.append(i)
+    inverted_index.append(inverted_unit.InvertedUnit(doclist))
 
-inverted_file = open('./data/inverted_index_cache.pkl','wb')
-pkl.dump(inverted_index,inverted_file)
+inverted_file = open('./data/inverted_index_cache.pkl', 'wb')
+pkl.dump(inverted_index, inverted_file)
 inverted_file.close()
-	
 
 for i in range(wordnum):
-	idf[i] = math.log((1+dictnum)/idf[i])
+    idf[i] = math.log((1 + dictnum) / idf[i])
 
 for i in range(dictnum):
-	for lemma in dictlist[i].tf_count.keys():
-		tfidf[i][wordnumber[lemma]] = dictlist[i].tf_count[lemma]*idf[wordnumber[lemma]]
+    for lemma in dictlist[i].tf_count.keys():
+        tfidf[i][wordnumber[lemma]] = dictlist[i].tf_count[lemma] * idf[wordnumber[lemma]]
 
-idf_file = open('./data/idf_cache.pkl','wb')
-pkl.dump(idf,idf_file)
+idf_file = open('./data/idf_cache.pkl', 'wb')
+pkl.dump(idf, idf_file)
 idf_file.close()
 
-tfidf_file = open('./data/tfidf_cache.pkl','wb')
-pkl.dump(tfidf,tfidf_file)
+tfidf_file = open('./data/tfidf_cache.pkl', 'wb')
+pkl.dump(tfidf, tfidf_file)
 tfidf_file.close()
-
-
-
-
-
-	
-	
-
-
-
-
-
